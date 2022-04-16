cbef91008c658b58c912a05908a8813104a5ec7b
Brian Fischer <brian@fischfooddesigns.com>
2022-04-16 02:05:08 -0400

    improve: comments to define variables and placeholders

--- js/connect.js
+++ js/connect.js
@@ -1,5 +1,9 @@
+// Your username, case sensitive
 const channel = 'username';
 
+// bot_username = Username of the bot outputting the messages in chat
+// bot_access_token = Authentication token needed to connect to Twitch
+// [channel] = Pulls from the channel defined above
 const client = new tmi.Client({
   connection: {
     secure: true,
